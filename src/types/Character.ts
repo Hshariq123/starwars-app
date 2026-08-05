de78fa4fe0ac8b744155635cc2b097155fbd07ca
Shariq Hussain <shariqhussain361@gmail.com>
2026-08-05 13:03:09 +0530

    Fetch and display Star Wars characters

---
+++ src/types/Character.ts
@@ -0,0 +1,11 @@
+export interface Character {
+  name: string;
+  height: string;
+  mass: string;
+  birth_year: string;
+  created: string;
+  homeworld: string;
+  films: string[];
+  species: string[];
+  url: string;
+}
