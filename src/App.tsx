d38661018f85dfb9fd700669a3c86acdbaf61a96
Shariq Hussain <shariqhussain361@gmail.com>
2026-08-05 13:41:13 +0530

    Implement client-side pagination

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,22 @@ function App() {
   const [characters, setCharacters] = useState<Character[]>([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState("");
+
+  const [currentPage, setCurrentPage] = useState(1);
+  const charactersPerPage = 10;
+
+  const lastCharacterIndex = currentPage * charactersPerPage;
+  const firstCharacterIndex = lastCharacterIndex - charactersPerPage;
+
+  const currentCharacters = characters.slice(
+    firstCharacterIndex,
+    lastCharacterIndex,
+  );
+  const totalPages = Math.ceil(characters.length / charactersPerPage);
+  const pageNumbers = Array.from(
+    { length: totalPages },
+    (_, index) => index + 1,
+  );
   useEffect(() => {
     const fetchCharacters = async () => {
       try {
@@ -33,9 +49,46 @@ function App() {
     <div>
       <h1>Star Wars Characters</h1>
 
-      {characters.map((character) => (
+      {currentCharacters.map((character) => (
         <p key={character.url}>{character.name}</p>
       ))}
+      <div>
+        <button
+          onClick={() => setCurrentPage((prev) => prev - 1)}
+          disabled={currentPage === 1}
+          style={{ padding: "5px" }}
+        >
+          Previous
+        </button>
+
+        {pageNumbers.map((pageNumber) => (
+          <button
+            key={pageNumber}
+            onClick={() => setCurrentPage(pageNumber)}
+            style={{
+              fontWeight: currentPage === pageNumber ? "bold" : "normal",
+              backgroundColor:
+                currentPage === pageNumber ? "#2563eb" : "#ffffff",
+              color: currentPage === pageNumber ? "#ffffff" : "#000000",
+              margin: "10px 4px",
+              padding: "6px 12px",
+              border: "1px solid #ccc",
+              borderRadius: "4px",
+              cursor: "pointer",
+            }}
+          >
+            {pageNumber}
+          </button>
+        ))}
+
+        <button
+          onClick={() => setCurrentPage((prev) => prev + 1)}
+          disabled={lastCharacterIndex >= characters.length}
+          style={{ padding: "5px" }}
+        >
+          Next
+        </button>
+      </div>
     </div>
   );
 }
